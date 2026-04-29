a6bdafffb325b9a7b12950c0b1a59a8630650f70
Norah als <none.als012@gmail.com>
2026-04-29 15:07:55 +0300

    Add task 4: doubly linked list delete function with correct implementation

---
+++ challenge/4-delete_dnodeint/delete_dnodeint_at_index.c
@@ -0,0 +1,42 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * delete_dnodeint_at_index - deletes the node at index index of a dlistint_t linked list
+ * @head: pointer to the head of the list
+ * @index: index of the node to delete
+ * Return: 1 if it succeeded, -1 if it failed
+ */
+int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
+{
+    dlistint_t *current;
+    unsigned int i;
+
+    if (head == NULL || *head == NULL)
+        return (-1);
+
+    current = *head;
+
+    if (index == 0)
+    {
+        *head = current->next;
+        if (*head != NULL)
+            (*head)->prev = NULL;
+        free(current);
+        return (1);
+    }
+
+    for (i = 0; current != NULL && i < index; i++)
+        current = current->next;
+
+    if (current == NULL)
+        return (-1);
+
+    if (current->prev != NULL)
+        current->prev->next = current->next;
+    if (current->next != NULL)
+        current->next->prev = current->prev;
+
+    free(current);
+    return (1);
+}
