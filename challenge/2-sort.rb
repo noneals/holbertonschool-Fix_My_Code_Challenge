60d5dae79ff08bfaa46d5a7a2b2017574d1913d8
Norah als <none.als012@gmail.com>
2026-04-29 15:03:27 +0300

    Fix task 2: correct insertion sort logic

--- challenge/2-sort.rb
+++ challenge/2-sort.rb
@@ -1,8 +1,31 @@
 ###
 #
-#  Sort integer arguments (ascending)
+#  Sort integer arguments (ascending) 
 #
 ###
 
-result = ARGV.filter { |arg| arg =~ /^-?[0-9]+$/ }.map(&:to_i).sort
-result.each { |value| puts value }
+result = []
+ARGV.each do |arg|
+    # skip if not integer
+    next if arg !~ /^-?[0-9]+$/
+
+    # convert to integer
+    i_arg = arg.to_i
+    
+    # insert result at the right position
+    is_inserted = false
+    i = 0
+    l = result.size
+    while !is_inserted && i < l do
+        if result[i] < i_arg
+            i += 1
+        else
+            result.insert(i, i_arg)
+            is_inserted = true
+            break
+        end
+    end
+    result << i_arg if !is_inserted
+end
+
+puts result
